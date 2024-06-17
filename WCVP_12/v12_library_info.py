700802b68984c5da8805ae3681c4fcba4b76b3ad
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-06-17 10:54:16 +0100

    Move all to v12 folder

--- WCVP_12/v12_library_info.py
+++ WCVP_12/v12_library_info.py
@@ -1,3 +1,2 @@
 WCVP_VERSION = '12'
 FAMILIES_IN_GENTIANALES = ['Apocynaceae', 'Loganiaceae', 'Gentianaceae', 'Gelsemiaceae', 'Rubiaceae']
-# MY_MAIN_FAMILIES = ['Apocynaceae', 'Loganiaceae', 'Rubiaceae']
